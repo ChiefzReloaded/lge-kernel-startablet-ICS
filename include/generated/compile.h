a58cffc1360bcc5959766915ae5b58af886bb8ac
Ryan Scott <chiefz.reloaded@gmail.com>
2012-01-13 16:24:04 -0800

    initial commit of ICS kernel src

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 7 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#7 SMP PREEMPT Sat Jan 7 19:36:18 PST 2012"
+#define LINUX_COMPILE_TIME "19:36:18"
+#define LINUX_COMPILE_BY "CRF-Team"
+#define LINUX_COMPILE_HOST "BawseMod"
+#define LINUX_COMPILE_DOMAIN "linuxmint-domain"
+#define LINUX_COMPILER "gcc version 4.4.0 (GCC) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "2.6.36.3 ChiefzReloaded-CFQ-DLIO-CIFS-ICS-4.0.3++"
